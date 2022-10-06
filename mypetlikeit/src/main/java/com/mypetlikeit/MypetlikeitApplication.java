0b9a94a3bf8ef1387eb23e0f70d777af5b143e2b
meeejyu <alwn04016@naver.com>
2022-10-06 17:11:14 +0900

    프로젝트 init

---
+++ mypetlikeit/src/main/java/com/mypetlikeit/MypetlikeitApplication.java
@@ -0,0 +1,13 @@
+package com.mypetlikeit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MypetlikeitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MypetlikeitApplication.class, args);
+	}
+
+}
---
+++ mypetlikeit/src/test/java/com/mypetlikeit/MypetlikeitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mypetlikeit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MypetlikeitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
