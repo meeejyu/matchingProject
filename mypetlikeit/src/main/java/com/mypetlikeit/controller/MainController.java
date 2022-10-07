563c87fd74db0f337140ab0e8a1c9365286b94c1
meeejyu <alwn04016@naver.com>
2022-10-07 23:01:35 +0900

    main, login, sigup 페이지 추가 (진행중)

---
+++ mypetlikeit/src/main/java/com/mypetlikeit/controller/MainController.java
@@ -0,0 +1,25 @@
+package com.mypetlikeit.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "/")
+public class MainController {
+    
+    @GetMapping("/main")
+    public String main() {
+        return "main";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/signup")
+    public String signUp() {
+        return "signUp";
+    }
+}
